6631b43fca6937c693aca732f15a97a418b67b47
Heem Dev <40331543+Heem-dev@users.noreply.github.com>
2025-08-03 09:19:55 +0300

    font added. styling top bar is done.

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,21 @@ function App() {
   return (
     <>
       <div className="mainContainer flex flex-col w-[100vw] h-[100vh]">
-        <div className="titleContainer bg-[#b3a125] p-2 pb-3 w-[100%] text-center h-fit">
+        <div className="titleContainer bg-background p-2 pb-3 w-[100%] text-center h-fit">
           {pageTitle}
         </div>
-        <div className="infoContainer bg-[#b3a125] ">
+        <div className="infoContainer bg-background ">
           <div className="gameDescription">
             <div className="descriptionText text-center">
               Try to click all images only once!
             </div>
           </div>
           <div className="scoresContainer mt-2 mb-2 flex flex-row justify-around">
-            <div className="currentScore">Current Score: 2</div>
+            <div className="currentScore text-primary">Current Score: 2</div>
             <div className="bestScore">Best Score: 12</div>
           </div>
         </div>
-        <div className="gameContainer bg-[#3b4cca] h-[100%]"></div>
+        <div className="gameContainer bg-third h-[100%]"></div>
         <div className="footer"></div>
       </div>
     </>
