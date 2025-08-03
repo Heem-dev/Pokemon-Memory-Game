0035ea08abe1a6f0a1699122076cfda68b4ec945
Heem Dev <40331543+Heem-dev@users.noreply.github.com>
2025-08-04 00:14:33 +0300

    displaying cards on grid done.

--- src/componenets/cardsGrid.jsx
+++ src/componenets/cardsGrid.jsx
@@ -4,7 +4,7 @@ export default function CardsGrid({ pokemonData }) {
   return (
     <>
       {pokemonData.map((card) => {
-        // Extract ID from URL
+        // Extract poke ID from URL
         const pokeID = card.url.match(/\/(\d+)\//)[1]
 
         const imgLink = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeID}.png`
